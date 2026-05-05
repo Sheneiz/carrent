aecbf2aa107ee52fe267446a25e9a8f492cad237
Grzegorz Kozyra <78079522+Sheneiz@users.noreply.github.com>
2026-05-05 13:22:16 +0200

    Hardcode database URL in DatabaseConnection
    Removed commented code regarding environment variable usage.

--- src/main/java/carrent/db/DatabaseConnection.java
+++ src/main/java/carrent/db/DatabaseConnection.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     public static Connection getConnection() throws SQLException {
-        // Zamiast zmiennej środowiskowej, wklejamy URL prosto w kod
         String dbUrl = "jdbc:postgresql://ep-red-surf-amzkrdd2-pooler.c-5.us-east-1.aws.neon.tech/neondb?user=neondb_owner&password=npg_fY6QqhRV4BMP&sslmode=require&channel_binding=require";
         
         return DriverManager.getConnection(dbUrl);
