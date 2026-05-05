e3ef91d849875f67fd954281f40ce06826a179f3
Grzegorz Kozyra <78079522+Sheneiz@users.noreply.github.com>
2026-05-05 13:25:13 +0200

    Update Main.java

--- src/main/java/carrent/Main.java
+++ src/main/java/carrent/Main.java
@@ -16,7 +16,7 @@ import carrent.services.VehicleValidator;
 
 public class Main {
     public static void main(String[] args) {
-        String storageType = args.length > 0 ? args[0].toLowerCase() : "jdbc";
+        String storageType = "jdbc";
 
         UserRepository userRepo;
         VehicleRepository vehicleRepo;
