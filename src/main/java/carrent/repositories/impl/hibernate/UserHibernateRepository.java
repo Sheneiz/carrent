5dc678063d449e5b32ef90b9e41b20b21bdf9114
Grzegorz <grzesiek03.gk@gmail.com>
2026-06-14 23:36:14 +0200

    tbd

--- src/main/java/carrent/repositories/impl/hibernate/UserHibernateRepository.java
+++ src/main/java/carrent/repositories/impl/hibernate/UserHibernateRepository.java
@@ -7,8 +7,9 @@ import java.util.Optional;
 
 @org.springframework.stereotype.Repository
 @org.springframework.context.annotation.Profile("hibernate")
+@org.springframework.transaction.annotation.Transactional
 public class UserHibernateRepository implements UserRepository {
-    
+
     @jakarta.persistence.PersistenceContext
     private jakarta.persistence.EntityManager entityManager;
 
@@ -16,20 +17,24 @@ public class UserHibernateRepository implements UserRepository {
     public List<User> findAll() {
         return entityManager.createQuery("FROM User", User.class).getResultList();
     }
+
     @Override
     public Optional<User> findById(String id) {
         return Optional.ofNullable(entityManager.find(User.class, id));
     }
+
     @Override
     public Optional<User> findByLogin(String login) {
         jakarta.persistence.TypedQuery<User> query = entityManager.createQuery("FROM User WHERE login = :login", User.class);
         query.setParameter("login", login);
         return query.getResultStream().findFirst();
     }
+
     @Override
     public User save(User user) {
         return entityManager.merge(user);
     }
+
     @Override
     public void deleteById(String id) {
         User user = entityManager.find(User.class, id);
